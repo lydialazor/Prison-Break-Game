e1684f8b7db8564bdeb715140333968618cc8684
achandra85@gatech.edu <ankithchandra@lawn-128-61-53-158.lawn.gatech.edu>
2023-02-24 12:05:55 -0500

    adding player movement

--- app/src/main/java/com/example/prison_break/GameScreen.java
+++ app/src/main/java/com/example/prison_break/GameScreen.java
@@ -2,8 +2,13 @@ package com.example.prison_break;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
@@ -13,6 +18,10 @@ public class GameScreen extends AppCompatActivity {
     private TextView difficulty;
     private ImageButton playerChoice;
     private Button add;
+
+    private ImageView square;
+    private int squareX, squareY;
+    private int prevX, prevY;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -30,18 +39,97 @@ public class GameScreen extends AppCompatActivity {
         String str1 = intent1.getStringExtra("Difficulty: ");
         difficulty.setText(str1);
 
-        playerChoice = findViewById(R.id.playerChoice);
         Intent intent2 = getIntent();
         String str2 = intent2.getStringExtra("Player");
+
+        square = findViewById(R.id.square);
         if (str2.equals("dude1")) {
-            playerChoice.setImageResource(R.drawable.dude1);
+            square.setImageResource(R.drawable.dude1);
         } else if (str2.equals("dude2")) {
-            playerChoice.setImageResource(R.drawable.dude2);
+            square.setImageResource(R.drawable.dude2);
         } else if (str2.equals("dude3")) {
-            playerChoice.setImageResource(R.drawable.dude3);
+            square.setImageResource(R.drawable.dude3);
         }
+        square.setOnTouchListener(this::onTouch);
+        square.setOnKeyListener(this::onKey);
+        square.setFocusable(true);
+        square.setFocusableInTouchMode(true);
+        // get the dimensions of the screen
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int screenWidth = metrics.widthPixels;
+        int screenHeight = metrics.heightPixels;
+        // initialize the position of the square to the middle of the bottom 100 pixels of the screen
+        squareX = (screenWidth - square.getWidth()) / 2;
+        squareY = 1650;
+        square.setX(squareX);
+        square.setY(squareY);
+
+        // initialize the pos of grass to bottom middle;
+        View grass = findViewById(R.id.grass);
+        //int grassX = (screenWidth - 1100); //  - square.getWidth()) / 2 - 700
+        int grassY = screenHeight - 1000 - square.getHeight();
+        grass.setX(0);
+        grass.setY(grassY - 300);
+
 
     }
 
-    
-}
+    public boolean onTouch(View view, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                prevX = (int) event.getX();
+                prevY = (int) event.getY();
+                squareX = (int) square.getX();
+                squareY = (int) square.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int newX = (int) event.getX();
+                int newY = (int) event.getY();
+                int deltaX = newX - prevX;
+                int deltaY = newY - prevY;
+                square.setX(squareX + deltaX);
+                square.setY(squareY + deltaY);
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+
+    public boolean onKey(View view, int keyCode, KeyEvent event) {
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_DPAD_UP:
+                if (squareY > 0) {
+                    square.setY(squareY - 100);
+                    squareY -= 100;
+                }
+                break;
+            case KeyEvent.KEYCODE_DPAD_DOWN:
+                if (squareY < 1650) { // view.getHeight() - square.getHeight()
+                    square.setY(squareY + 100);
+                    squareY += 100;
+                }
+                break;
+            case KeyEvent.KEYCODE_DPAD_LEFT:
+                if (squareX > 0) {
+                    square.setX(squareX - 100);
+                    squareX -= 100;
+                }
+                break;
+            case KeyEvent.KEYCODE_DPAD_RIGHT:
+                if (squareX < view.getWidth() - square.getWidth() + 850) {
+                    square.setX(squareX + 100);
+                    squareX += 100;
+                }
+                break;
+            default:
+                return false;
+        }
+
+        return true;
+    }
+
+    }
+
+
