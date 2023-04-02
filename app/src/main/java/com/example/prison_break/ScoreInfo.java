3b62d88a4e2276cfadbb19dc17cba7bd21a3e02e
Lazor, Lydia Alexandra <llazor@gatech.edu>
2023-04-02 15:57:32 -0400

    Fixing restart button

--- app/src/main/java/com/example/prison_break/ScoreInfo.java
+++ app/src/main/java/com/example/prison_break/ScoreInfo.java
@@ -6,28 +6,6 @@ public class ScoreInfo {
     protected static int scoreTracker = 0;
     private static int numLives;
 
-    public static void calculateScore() {
-        if (ScoreInfo.getTracker() > Player.getY()) {
-            if (Player.getY() < 1501 && Player.getY() > 1150) {
-                ScoreInfo.setPoints(10);
-                System.out.println("Scoreinfo");
-            } else if (Player.getY() <= 1150 && Player.getY() > 950) {
-                ScoreInfo.setPoints(5);
-            } else if (Player.getY() <= 950 && Player.getY() > 750) {
-                ScoreInfo.setPoints(15);
-            } else if (Player.getY() <= 750 && Player.getY() > 550) {
-                ScoreInfo.setPoints(5);
-            } else if (Player.getY() <= 550 && Player.getY() > 250) {
-                ScoreInfo.setPoints(10);
-            } else if (Player.getY() <= 250 && Player.getY() > 150) {
-                ScoreInfo.setPoints(5);
-            } else {
-                ScoreInfo.setPoints(25);
-            }
-            ScoreInfo.setTracker(Player.getY());
-        }
-    }
-
     // static methods
     public static void setPoints(int x) {num += x;}
     public void resetPoints() {
@@ -56,6 +34,30 @@ public class ScoreInfo {
     public static int getTracker() {
         return tracker;
     }
+
+    public static void calculateScore() {
+        if (ScoreInfo.getTracker() > Player.getY()) {
+            if (Player.getY() < 1501 && Player.getY() > 1150) {
+                ScoreInfo.setPoints(10);
+                System.out.println("Scoreinfo");
+            } else if (Player.getY() <= 1150 && Player.getY() > 950) {
+                ScoreInfo.setPoints(5);
+            } else if (Player.getY() <= 950 && Player.getY() > 750) {
+                ScoreInfo.setPoints(15);
+            } else if (Player.getY() <= 750 && Player.getY() > 550) {
+                ScoreInfo.setPoints(5);
+            } else if (Player.getY() <= 550 && Player.getY() > 250) {
+                ScoreInfo.setPoints(10);
+            } else if (Player.getY() <= 250 && Player.getY() > 150) {
+                ScoreInfo.setPoints(5);
+            } else {
+                ScoreInfo.setPoints(25);
+            }
+            ScoreInfo.setTracker(Player.getY());
+        }
+    }
+
+
 
 }
 
